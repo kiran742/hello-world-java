413838241ec90476cd412416986dbe648a32d232
root <root@ip-172-31-23-66.us-east-2.compute.internal>
2020-07-16 04:46:41 +0000

    updated the compile error

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,7 +35,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("Hi corona";
+    System.out.println("Hi corona");
     System.out.println("bye corona");
 
   }
